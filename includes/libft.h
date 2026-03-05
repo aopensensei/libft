8ced8c70178b93cc201fd62766c08c28339b170b
tmaeda <tmaeda@student.42tokyo.jp>
2026-03-05 17:22:53 +0900

    update header

--- includes/libft.h
+++ includes/libft.h
@@ -7,5 +7,8 @@ size_t	ft_strlen(const char *str);
 int	ft_isdigit(int ch);
 int	ft_isalpha(int ch);
 int	ft_isalnum(int ch);
-
+int	ft_isascii(int ch);
+int	ft_isprint(int ch);
+int	ft_toupper(int ch);
+int	ft_tolower(int ch);
 #endif
