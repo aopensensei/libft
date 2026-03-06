e698051fd60a7b7ac972d1e35a5782da5767f53a
tmaeda <tmaeda@student.42tokyo.jp>
2026-03-06 12:51:42 +0900

    update ft_strchr.c

--- src/ft_strchr.c
+++ src/ft_strchr.c
@@ -8,10 +8,10 @@ char	*ft_strchr(const char *str, int ch)
 	while (str[i])
 	{
 		if (str[i] == (char)ch)
-			return (&str[i]);
+			return ((char *)&str[i]);
 		i++;
 	}
 	if (ch == '\0')
-		return (&str[i]);
+		return ((char *)&str[i]);
 	return (NULL);
 }
