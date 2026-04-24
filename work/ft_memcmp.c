c3455c05c0d191853e908a7de87ec7e07f7f6569
Takamasa Maeda <tmaeda@c5r2s8.42tokyo.jp>
2026-04-24 22:25:49 +0900

    add memcmp

---
+++ work/ft_memcmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmaeda <tmaeda@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/24 22:20:58 by tmaeda            #+#    #+#             */
+/*   Updated: 2026/04/24 22:21:00 by tmaeda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*uc_s1;
+	unsigned char	*uc_s2;
+
+	uc_s1 = (unsigned char *)s1;
+	uc_s2 = (unsigned char *)s2;
+	while (n--)
+	{
+		if (*uc_s1 != *uc_s2)
+			return (*uc_s1 - *uc_s2);
+		uc_s1++;
+		uc_s2++;
+	}
+	return (0);
+}
